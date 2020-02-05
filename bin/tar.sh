96895937d9ad0f8c10d80835d7d06336e3b94bc1
sahe417 <41294520+sahe417@users.noreply.github.com>
2020-02-05 23:56:57 +0900

    ..

--- bin/tar.sh
+++ bin/tar.sh
@@ -4,7 +4,7 @@ LANG=C
 PATH=/usr/local/bin:/bin:/usr/bin
 export LANG PATH
 
-test $# -ge 1 || exit
+test $# -ge 1 || exit 1
 
 ( find ${1+"$@"} -type d | sed -e 's/$/\//'
   find ${1+"$@"} \! -type d ) | sort | tar -cf - --no-recursion -T -
