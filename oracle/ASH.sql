4c1c65aeb0882c9079abaa8a7875ae41c4358c4f
vagrant <sahe417suna@gmail.com>
2020-11-29 06:14:46 +0000

    構成変更

--- oracle/ASH.sql
+++ oracle/ASH.sql
@@ -6,16 +6,13 @@ set feed off
 set echo off
 set colsep ','
 
-
 -- NLS_DATE_FORMAT
 ALTER SESSION SET NLS_DATE_FORMAT = 'yyyy/mm/dd hh24:mi:ss';
-
 
 -- input timestamp
 ACCEPT s_report_date CHAR PROMPT 'Enter report date (yyyy-mm-dd): '
 ACCEPT s_stt CHAR PROMPT 'Enter start time (HH24:MI): '
 ACCEPT s_edt CHAR PROMPT 'Enter end time (HH24:MI): '
-
 
 -- v$active_session_history
 spool v_ash_&s_report_date
@@ -25,7 +22,6 @@ AND sample_time < TO_DATE('&s_report_date' || ' ' || '&s_edt' || ':00', 'yyyy-mm
 ORDER BY sample_time, session_id, session_serial#;
 spool off
 
-
 -- dba_hist_active_sess_history
 spool d_ash_&s_report_date
 SELECT * FROM dba_hist_active_sess_history
@@ -33,7 +29,6 @@ WHERE sample_time >= TO_DATE('&s_report_date' || ' ' || '&s_stt' || ':00', 'yyyy
 AND sample_time < TO_DATE('&s_report_date' || ' ' || '&s_edt' || ':00', 'yyyy-mm-dd HH24:MI:SS')
 ORDER BY sample_time, session_id, session_serial#;
 spool off
-
 
 -- undefine
 undefine s_report_date
